d90540ba3c84130aee8fbce5106bed5c967f3258
JobGetabu <getabujob@gmail.com>
2018-07-14 00:34:40 +0300

    works fine with photo detail

--- app/src/main/java/com/job/anthologyweddingphotographyapp/PhotoDetail.java
+++ app/src/main/java/com/job/anthologyweddingphotographyapp/PhotoDetail.java
@@ -1,7 +1,11 @@
 package com.job.anthologyweddingphotographyapp;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import static com.job.anthologyweddingphotographyapp.MainActivity.IMEXTRA;
 
 public class PhotoDetail extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class PhotoDetail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_detail);
+
+        //init
+        ImageView imageView = findViewById(R.id.photo_im);
+
+        WeddingImage image = (WeddingImage) getIntent().getParcelableExtra(IMEXTRA);
+
+        imageView.setImageDrawable(ContextCompat.getDrawable(this,image.getImgRes()));
+
     }
 }
