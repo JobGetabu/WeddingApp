af3f9c53bf6f6abc478e9a30abbcd772ba227c2b
JobGetabu <getabujob@gmail.com>
2018-07-13 23:59:02 +0300

    added photo details activity

---
+++ app/src/main/java/com/job/anthologyweddingphotographyapp/PhotoDetail.java
@@ -0,0 +1,13 @@
+package com.job.anthologyweddingphotographyapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhotoDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_detail);
+    }
+}
